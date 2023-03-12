6cc564449afe8a8f2f2f1aa83e886da24de3a0cf
Edmilbe <edmilbe14@hotmail.com>
2023-03-12 21:28:34 +0000

    Hide google and facebook login

--- src/component/login/Common.js
+++ src/component/login/Common.js
@@ -1,11 +1,11 @@
-import React, { useState } from 'react';
-import { ImFacebook, ImGoogle } from 'react-icons/im';
+import React, { useState } from "react";
+import { ImFacebook, ImGoogle } from "react-icons/im";
 
 //internal import
-import Login from '@component/login/Login';
-import Register from '@component/login/Register';
-import ResetPassword from '@component/login/ResetPassword';
-import useLoginSubmit from '@hooks/useLoginSubmit';
+import Login from "@component/login/Login";
+import Register from "@component/login/Register";
+import ResetPassword from "@component/login/ResetPassword";
+import useLoginSubmit from "@hooks/useLoginSubmit";
 
 const Common = ({ setModalOpen }) => {
   const [showRegister, setShowRegister] = useState(false);
@@ -42,7 +42,7 @@ const Common = ({ setModalOpen }) => {
           OR
         </div>
 
-        <div className="flex justify-between flex-col lg:flex-row">
+        {/* <div className="flex justify-between flex-col lg:flex-row">
           <button className="text-sm inline-flex items-center cursor-pointer transition ease-in-out duration-300 font-semibold font-serif text-center justify-center rounded-md focus:outline-none text-gray-600 bg-gray-100 shadow-sm md:px-2 my-1 sm:my-1 md:my-1 lg:my-0 lg:px-3 py-4 md:py-3.5 lg:py-4 hover:text-white hover:bg-blue-600 h-11 md:h-12 w-full mr-2">
             <ImFacebook /> <span className="ml-2">Login With Facebook</span>
           </button>
@@ -62,15 +62,15 @@ const Common = ({ setModalOpen }) => {
             onFailure={handleGoogleSignIn}
             cookiePolicy={'single_host_origin'}
           />
-        </div>
+        </div> */}
         <div className="text-center text-sm text-gray-900 mt-4">
           <div className="text-gray-500 mt-2.5">
-            {showRegister ? 'Already have a account ?' : 'Not have a account ?'}
+            {showRegister ? "Already have a account ?" : "Not have a account ?"}
             <button
               onClick={handleModal}
               className="text-gray-800 hover:text-emerald-500 font-bold mx-2"
             >
-              {showRegister ? 'Login' : 'Register'}
+              {showRegister ? "Login" : "Register"}
             </button>
           </div>
         </div>
